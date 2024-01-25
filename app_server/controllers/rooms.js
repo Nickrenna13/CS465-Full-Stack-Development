738c9b1b1e449e0ea7d8e446512a201ca4ada8dd
Nicholas <nicholas.renna@snhu.edu>
2024-01-25 15:49:29 -0700

    Convert Rooms template from static data to dynamic JSON

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
+const rent = JSON.parse(fs.readFileSync('./data/rent.json', 'utf8'));
 /* GET rooms */ 
 const rooms = (req, res) => {
-    res.render('rooms', {title: 'Rooms Travlr'});
+    res.render('rooms', {title: 'Rooms Travlr', rent});
 };
 
 module.exports = {
