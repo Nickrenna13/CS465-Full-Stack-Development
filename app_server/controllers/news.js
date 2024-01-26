b3593cd6ffd8a256381620861b763df0b320a49f
Nicholas <nicholas.renna@snhu.edu>
2024-01-26 15:18:57 -0700

    Convert News template from static data to dynamic JSON

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const story = JSON.parse(fs.readFileSync('./data/story.json', 'utf8'));
+const latest_news = JSON.parse(fs.readFileSync('./data/latest_news.json', 'utf8'));
+const vac_tips = JSON.parse(fs.readFileSync('./data/vac_tips.json', 'utf8'));
 /* GET news */ 
 const news = (req, res) => {
-    res.render('news', {title: 'News Travlr'});
+    res.render('news', {title: 'News Travlr', story, latest_news, vac_tips});
 };
 
 module.exports = {
