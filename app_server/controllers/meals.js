6b67b3c0c8abd72aecb1921f7d6d0dc311283a64
Nicholas <nicholas.renna@snhu.edu>
2024-01-27 16:42:13 -0700

    Converted meals template from static data to dynamic  JSON

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
+const food = JSON.parse(fs.readFileSync('./data/food.json', 'utf8'));
 /* GET meals */ 
 const meals = (req, res) => {
-    res.render('meals', {title: 'Meals Travlr'});
+    res.render('meals', {title: 'Meals Travlr', food});
 };
 
 module.exports = {
