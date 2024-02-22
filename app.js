4a39557c92786b6c24bf99edeb7e9b1d08445339
Nicholas <nicholas.renna@snhu.edu>
2024-02-22 08:38:23 -0700

    Fixed a user error

--- app.js
+++ app.js
@@ -59,8 +59,13 @@ app.use('/news', newsRouter);
 app.use('/rooms', roomsRouter);
 
 app.use('/api', apiRouter);
-
-
+app.use(function(err, req, res, next){
+  if(err.name === "UnauthorizedError"){
+    res
+      .status(401)
+      .json({ "message": err.name + ": " + err.message });
+  }
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Model = mongoose.model("trips");
-const User = mongoose.model('users');
+const user = mongoose.model("users");
 
 const tripsList = async (req, res) => {
   Model.find({}).exec((err, trips) => {
@@ -28,73 +28,72 @@ const tripsFindByCode = async (req, res) => {
 
 const tripsAddTrip = async (req, res) => {
   getUser(req, res, (req, res) => {
-  Model.create(
-    {
-      code: req.body.code,
-      name: req.body.name,
-      length: req.body.length,
-      start: req.body.start,
-      resort: req.body.resort,
-      perPerson: req.body.perPerson,
-      image: req.body.image,
-      description: req.body.description,
-    },
-    (err, trip) => {
-      if (err) {
-        return res
-          .status(400) //bad request
-          .json(err);
-      } else {
-        return res
-          .status(201) //creates
-          .json(trip);
+    Model.create(
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      },
+      (err, trip) => {
+        if (err) {
+          return res
+            .status(400) //bad request
+            .json(err);
+        } else {
+          return res
+            .status(201) //creates
+            .json(trip);
+        }
       }
-    }
-  );
+    );
   });
-}
+};
 
 const tripsUpdateTrip = async (req, res) => {
   getUser(req, res, (req, res) => {
-  console.log(req.body);
-  Model.findOneAndUpdate(
-    { code: req.params.tripCode },
-    {
-      code: req.body.code,
-      name: req.body.name,
-      length: req.body.length,
-      start: req.body.start,
-      resort: req.body.resort,
-      perPerson: req.body.perPerson,
-      image: req.body.image,
-      description: req.body.description,
-    },
-    { new: true }
-  )
-    .then((trip) => {
-      if (!trip) {
-        return res.status(404).send({
-          message: "Trip not found with code " + req.params.tripCode,
-        });
-      }
-      res.send(trip);
-    })
-    .catch((err) => {
-      if (err.kind === "ObjectId") {
-        return res.status(404).send({
-          message: "Trip not found with code " + req.params.tripCode,
-        });
-      }
-      return res
-        .status(500) // server error
-        .json(err);
-    });
+    Model.findOneAndUpdate(
+      { code: req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      },
+      { new: true }
+    )
+      .then((trip) => {
+        if (!trip) {
+          return res.status(404).send({
+            message: "Trip not found with code " + req.params.tripCode,
+          });
+        }
+        res.send(trip);
+      })
+      .catch((err) => {
+        if (err.kind === "ObjectId") {
+          return res.status(404).send({
+            message: "Trip not found with code " + req.params.tripCode,
+          });
+        }
+        return res
+          .status(500) // server error
+          .json(err);
+      });
   });
-}
+};
 
 const getUser = (req, res, callback) => {
   if (req.auth && req.auth.email) {
-    User.findOne({ email: req.auth.email }).exec((err, user) => {
+    user.findOne({ email: req.auth.email }).exec((err, user) => {
       if (!user) {
         return res.status(404).json({ message: "User not found1" });
       } else if (err) {
